f70812095f592e86fe88f574cf68e2c1d33fc4e0
Shakhzod Niyozov <shakhzod.n30@gmail.com>
2024-03-15 09:31:02 +0500

    Add Tag and TagConfiguration

--- src/Infrastructure/Data/MongoDbContext.cs
+++ src/Infrastructure/Data/MongoDbContext.cs
@@ -1,12 +1,10 @@
 using Microsoft.Extensions.Options;
-
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.IdGenerators;
 using MongoDB.Driver;
-
 using MRA.AssetsManagement.Application.Data;
 using MRA.AssetsManagement.Domain.Entities;
 using MRA.AssetsManagement.Infrastructure.Data.Configurations;
+
+using Tag = MRA.AssetsManagement.Domain.Entities.Tag;
 
 namespace MRA.AssetsManagement.Infrastructure.Data;
 
@@ -18,9 +16,12 @@ public class MongoDbContext : IApplicationDbContext
         IMongoDatabase database = client.GetDatabase(options.Value.DatabaseName);
         
         var assetConfig = new AssetTypeConfiguration(database);
+        var tagConfig = new TagConfiguration(database);
         var mongoRepository = new MongoRepository<AssetType>(assetConfig.Collection);
         AssetTypes = mongoRepository;
+        Tags = new MongoRepository<Tag>(tagConfig.Collection);
     }
 
     public IRepository<AssetType> AssetTypes { get; }
+    public IRepository<Tag> Tags { get; }
 }
