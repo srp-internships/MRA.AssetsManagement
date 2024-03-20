eed429c2bbc0f3251516bb51b945f3082ca32d52
Manuchehr Haidarov <manu929153666@gmail.com>
2024-03-20 18:02:46 +0500

    AssetTypes Detail page

---
+++ src/Web/Client/Services/AssetTypes/IAssetTypesService.cs
@@ -0,0 +1,13 @@
+﻿using MRA.AssetsManagement.Web.Client.Pages.Settings;
+
+namespace MRA.AssetsManagement.Web.Client.Services.AssetTypes
+{
+    public interface IAssetTypesService
+    {
+        event Action OnChange;
+        Task<List<AssetType>> GetAssetTypes();
+        Task<AssetType> GetAssetTypeById(string id);
+
+        string GenerateShortName(string name);
+    }
+}
---
+++ src/Web/Shared/AssetType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.AssetsManagement.Web.Shared
+{
+    public class AssetType
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string ShortName { get; set; } = null!;
+        public string Icon { get; set; } = null!;
+        public bool Archived { get; set; }
+    }
+}
