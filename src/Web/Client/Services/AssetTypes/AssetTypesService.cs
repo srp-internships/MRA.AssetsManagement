50f85c2a72445d00962391966225b07e0e0e14ae
Abbos Sidiqov <abbosidiqov@outlook.com>
2024-03-27 12:35:35 +0500

    added base env

--- src/Web/Client/Services/AssetTypes/AssetTypesService.cs
+++ src/Web/Client/Services/AssetTypes/AssetTypesService.cs
@@ -1,4 +1,6 @@
-﻿using MRA.AssetsManagement.Web.Shared.AssetTypes;
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using MRA.AssetsManagement.Web.Shared.AssetTypes;
 using MRA.BlazorComponents.HttpClient.Services;
 using MRA.BlazorComponents.Snackbar.Extensions;
 
@@ -6,14 +8,16 @@ using MudBlazor;
 
 namespace MRA.AssetsManagement.Web.Client.Services.AssetTypes
 {
-    public class AssetTypesService(IHttpClientService httpClient, ISnackbar snackbar) : IAssetTypesService
+    public class AssetTypesService(IHttpClientService httpClient, ISnackbar snackbar, IWebAssemblyHostEnvironment environment) : IAssetTypesService
     {
         public event Action? OnChange;
         public IEnumerable<GetAssetType> AssetTypes { get; set; } = Enumerable.Empty<GetAssetType>();
+
+        private readonly string _baseAddress = environment.BaseAddress;
         
         public async Task<IEnumerable<GetAssetType>> GetAssetTypes()
         {
-            var response = await httpClient.GetFromJsonAsync<List<GetAssetType>>("http://localhost:5203/api/assettypes");
+            var response = await httpClient.GetFromJsonAsync<List<GetAssetType>>($"{_baseAddress}api/assettypes");
             snackbar.ShowIfError(response, "Error was occured.");
             AssetTypes = response.Result!;
             return AssetTypes;
@@ -21,7 +25,7 @@ namespace MRA.AssetsManagement.Web.Client.Services.AssetTypes
 
         public async Task<GetAssetType> GetAssetTypeById(string id)
         {
-            var response = await httpClient.GetFromJsonAsync<GetAssetType>($"http://localhost:5203/api/assettypes/{id}");
+            var response = await httpClient.GetFromJsonAsync<GetAssetType>($"{_baseAddress}api/assettypes/{id}");
             snackbar.ShowIfError(response, "Error was occured.");
             OnChange?.Invoke();
             return response.Result!;
@@ -29,28 +33,28 @@ namespace MRA.AssetsManagement.Web.Client.Services.AssetTypes
 
         public async Task Create(CreateAssetTypeRequest newAssetType)
         {
-            var response = await httpClient.PostAsJsonAsync("http://localhost:5203/api/assettypes", newAssetType);
+            var response = await httpClient.PostAsJsonAsync($"{_baseAddress}api/assettypes", newAssetType);
             await GetAssetTypes();
             OnChange?.Invoke();
         }
 
         public async Task Archive(string id)
         {
-            var response = await httpClient.PutAsJsonAsync($"http://localhost:5203/api/assettypes/archive/{id}", null!);
+            var response = await httpClient.PutAsJsonAsync($"{_baseAddress}api/assettypes/archive/{id}", null!);
             await GetAssetTypes();
             OnChange?.Invoke();
         }
 
         public async Task Restore(string id)
         {
-            var response = await httpClient.PutAsJsonAsync($"http://localhost:5203/api/assettypes/restore/{id}", null!);
+            var response = await httpClient.PutAsJsonAsync($"{_baseAddress}api/assettypes/restore/{id}", null!);
             await GetAssetTypes();
             OnChange?.Invoke();
         }
 
         public async Task Update(GetAssetType getAssetType)
         {
-            var response = await httpClient.PutAsJsonAsync("http://localhost:5203/api/assettypes", getAssetType);
+            var response = await httpClient.PutAsJsonAsync($"{_baseAddress}api/assettypes", getAssetType);
             await GetAssetTypes();
             OnChange?.Invoke();
 
