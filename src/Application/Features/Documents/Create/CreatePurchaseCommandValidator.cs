17022c7bfcb457e3349a848997c0ae7dd2112be2
Shakhzod Niyozov <shakhzod.n30@gmail.com>
2024-03-28 16:31:45 +0500

    Add validator for CreatePurchaseCommand

---
+++ src/Application/Features/Documents/Create/CreatePurchaseCommandValidator.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+
+namespace MRA.AssetsManagement.Application.Features.Documents.Create;
+
+public class CreatePurchaseCommandValidator : AbstractValidator<CreatePurchaseCommand>
+{
+    public CreatePurchaseCommandValidator()
+    {
+        RuleFor(x => x.Vendor)
+            .NotNull().NotEmpty()
+            .WithMessage("'Vendor' must not be NULL or EMPTY");
+    }
+}
+
+public class DocumentDetailValidator : AbstractValidator<CreateDocumentDetailCommand>
+{
+    public DocumentDetailValidator()
+    {
+        RuleFor(x => x.Price).Must(x => x >= 0);
+        RuleFor(x => x.Quantity).Must(x => x >= 0);
+        RuleFor(x => x.AssetTypeId).NotNull().NotEmpty();
+    }
+}
