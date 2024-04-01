23fd3174a5c9f503e3d36f925af989af81764808
Manuchehr Haidarov <94978061+Manu92915@users.noreply.github.com>
2024-04-01 18:18:01 +0500

    Tags page and SettingMenuItemList (#12)
    * Tags page and SettingMenuItemList
    
    * unused usings removed
    
    * best solution
    
    * event implementation
    
    * Refactore code
    
    * Fix update bug
    
    * few changes menuitemsListService, Mudskeleton
    
    * Change parametr name
    
    * Create custom skeleton
    
    * fixed skeleton
    
    * done
    
    ---------
    
    Co-authored-by: Abbos Sidiqov <abbosidiqov@outlook.com>

--- src/Web/Client/Services/AssetTypes/AssetTypesService.cs
+++ src/Web/Client/Services/AssetTypes/AssetTypesService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using MRA.AssetsManagement.Web.Client.Common.Extensions;
+using MRA.AssetsManagement.Web.Client.Components.MenuItems;
 using MRA.AssetsManagement.Web.Shared.AssetTypes;
 using MRA.BlazorComponents.HttpClient.Services;
 using MRA.BlazorComponents.Snackbar.Extensions;
@@ -10,53 +12,36 @@ namespace MRA.AssetsManagement.Web.Client.Services.AssetTypes
 {
     public class AssetTypesService(IHttpClientService httpClient, ISnackbar snackbar, IWebAssemblyHostEnvironment environment) : IAssetTypesService
     {
-        public event Action? OnChange;
-        public IEnumerable<GetAssetType> AssetTypes { get; set; } = Enumerable.Empty<GetAssetType>();
 
         private readonly string _baseAddress = environment.BaseAddress;
-        
-        public async Task<IEnumerable<GetAssetType>> GetAssetTypes()
-        {
-            var response = await httpClient.GetFromJsonAsync<List<GetAssetType>>($"{_baseAddress}api/assettypes");
-            snackbar.ShowIfError(response, "Error was occured.");
-            AssetTypes = response.Result!;
-            return AssetTypes;
-        }
 
         public async Task<GetAssetType> GetAssetTypeById(string id)
         {
             var response = await httpClient.GetFromJsonAsync<GetAssetType>($"{_baseAddress}api/assettypes/{id}");
             snackbar.ShowIfError(response, "Error was occured.");
-            OnChange?.Invoke();
             return response.Result!;
         }
 
-        public async Task Create(CreateAssetTypeRequest newAssetType)
+        public async Task<GetAssetType> Create(CreateAssetTypeRequest newAssetType)
         {
-            var response = await httpClient.PostAsJsonAsync($"{_baseAddress}api/assettypes", newAssetType);
-            await GetAssetTypes();
-            OnChange?.Invoke();
+            var response = await httpClient.PostAsJsonAsync<GetAssetType>($"{_baseAddress}api/assettypes", newAssetType);
+            snackbar.ShowIfError(response, "Error was occured.");
+            return response.Result!;
         }
-
-        public async Task Archive(string id)
-        {
-            var response = await httpClient.PutAsJsonAsync($"{_baseAddress}api/assettypes/archive/{id}", null!);
-            await GetAssetTypes();
-            OnChange?.Invoke();
-        }
-
-        public async Task Restore(string id)
-        {
-            var response = await httpClient.PutAsJsonAsync($"{_baseAddress}api/assettypes/restore/{id}", null!);
-            await GetAssetTypes();
-            OnChange?.Invoke();
-        }
-
+        
         public async Task Update(GetAssetType getAssetType)
         {
             var response = await httpClient.PutAsJsonAsync($"{_baseAddress}api/assettypes", getAssetType);
-            await GetAssetTypes();
-            OnChange?.Invoke();
+            snackbar.ShowIfError(response, "Error was occured.");
         }
+
+        public async Task<List<MenuItem>> Fetch()
+        {
+            var response = await httpClient.GetFromJsonAsync<List<GetAssetType>>($"{_baseAddress}api/assettypes");
+            snackbar.ShowIfError(response, "Error was occured.");
+
+            return response.Result!.Select(mi => mi.ToMenuItem()).ToList();
+        }
+        
     }
 }
