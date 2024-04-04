204732356dff35b8799d788e65b59b92c84ad502
Alisher Mavlonov <amavlonoffs@gmail.com>
2024-04-04 14:25:39 +0500

    [80] Corrected the mistake

---
+++ src/Application/Features/Employees/EmployeeProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MRA.AssetsManagement.Domain.Entities.Employee;
+using MRA.AssetsManagement.Web.Shared.Employees;
+
+namespace MRA.AssetsManagement.Application.Features.Employees;
+
+public class EmployeeProfile : Profile
+{
+    public EmployeeProfile()
+    {
+        CreateMap<CreateEmployeeRequest, Employee>();
+        CreateMap<Employee, CreateEmployeeRequest>();
+        CreateMap<Employee, EmployeeResponse>();
+        CreateMap<EmployeeResponse, Employee>()
+            .ForMember(d=>d.FirstName,o
+                =>o.MapFrom(s=>GetSubStringBeforeSpace(s.FullName)))
+            .ForMember(d=>d.LastName,o
+                =>o.MapFrom(s=>GetSubStringAfterSpace(s.FullName)))
+            .ForMember(d=>d.FullName,o
+                =>o.MapFrom(s=>s.FullName));
+    }
+    private string GetSubStringBeforeSpace(string input)
+    {
+        string[] parts = input.Split(' ');
+        return parts[0];
+    }
+    private string GetSubStringAfterSpace(string input)
+    {
+        string[] parts = input.Split(' ');
+        return parts[1];
+    }
+}
