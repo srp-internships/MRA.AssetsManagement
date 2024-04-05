1fdebc33b9392e4f35aeed08d212db8df14689ad
AlisherMavlonov <135858632+AlisherMavlonov@users.noreply.github.com>
2024-04-05 10:44:02 +0500

    Deleted DocumentsController.cs (#22)
    * Deleted DocumentsController.cs
    
    * 93 Resolved the conflict

--- src/Web/Server/Controllers/AssetController.cs
+++ src/Web/Server/Controllers/AssetController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MRA.AssetsManagement.Application.Features.Assets.Queries;
 using MRA.AssetsManagement.Application.Features.AssetSerials.Queries;
+using MRA.AssetsManagement.Application.Features.Documents.Create;
 using MRA.AssetsManagement.Domain.Entities;
 using MRA.AssetsManagement.Web.Shared.Assets;
 
@@ -24,5 +25,14 @@ public class AssetsController : ApiControllerBase
     public async Task<ActionResult<IEnumerable<GetAssetSerial>>> GetAssetSerials(CancellationToken cancellationToken)
     {
         return Ok(await Mediator.Send(new GetAssetSerialQuery(), cancellationToken));
+    }
+    [HttpPost("purchase")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<Document>> CreatePurchase(CreatePurchaseCommand command)
+    {
+        var document = await Mediator.Send(command);
+        return Ok(document);
     }
 }
