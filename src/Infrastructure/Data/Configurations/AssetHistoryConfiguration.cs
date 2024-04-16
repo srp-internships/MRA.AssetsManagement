caceda320f66bc9c752040facb00981622765fcd
Manuchehr Haidarov <manu929153666@gmail.com>
2024-04-16 15:59:52 +0500

    fields in lower case

--- src/Infrastructure/Data/Configurations/AssetHistoryConfiguration.cs
+++ src/Infrastructure/Data/Configurations/AssetHistoryConfiguration.cs
@@ -17,6 +17,14 @@ public class AssetHistoryConfiguration : BaseConfiguration<AssetHistory>
         classMap.MapMember(x => x.UserId).SetElementName("userId");
         classMap.MapMember(x => x.HistoryAssetSerial).SetElementName("serial");
         classMap.MapMember(x => x.DateTime).SetElementName("dateTime");
+
+        BsonClassMap.RegisterClassMap<HistoryAssetSerial>(initializer =>
+        {
+            initializer.MapMember(x => x.Serial).SetElementName("serial");
+            initializer.MapMember(x => x.Asset).SetElementName("asset");
+            initializer.MapMember(x => x.Status).SetElementName("status");
+            initializer.MapMember(x => x.Employee).SetElementName("employee");
+        });
     }
     
     protected override void Configure()
