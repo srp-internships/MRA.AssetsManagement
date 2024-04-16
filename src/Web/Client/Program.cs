10b9310a91510bbde466611352c6454fb9b1c769
Alisher Mavlonov <amavlonoffs@gmail.com>
2024-04-16 11:58:12 +0500

    95-Clear URL after login and fixed CallBack

--- src/Web/Client/Program.cs
+++ src/Web/Client/Program.cs
@@ -2,14 +2,12 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
 using MRA.AssetsManagement.Web.Client;
 using MRA.AssetsManagement.Web.Client.Services.AssetTypes;
 using MRA.AssetsManagement.Web.Client.Services.Icons;
 using MRA.AssetsManagement.Web.Client.Services.Tags;
 using MRA.AssetsManagement.Web.Client.Services.Employees;
 using MRA.AssetsManagement.Web.Client.Components.MenuItems;
-using MRA.AssetsManagement.Web.Client.Services.AuthService;
 using MRA.AssetsManagement.Web.Client.Services.Assets;
 using MRA.AssetsManagement.Web.Client.Services.Report;
 using MRA.AssetsManagement.Web.Client.Services.ReportService;
@@ -18,7 +16,7 @@ using MRA.BlazorComponents.HttpClient;
 
 using MudBlazor.Services;
 
-using CustomAuthStateProvider = MRA.AssetsManagement.Web.Client.CustomAuthStateProvider;
+using CustomAuthStateProvider = MRA.AssetsManagement.Web.Client.Services.AuthService.CustomAuthStateProvider;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -41,7 +39,6 @@ builder.Services.AddScoped<IAssetTypesService, AssetTypesService>();
 builder.Services.AddScoped<ITagsService, TagsService>();
 builder.Services.AddScoped<IAssetsService, AssetsService>();
 builder.Services.AddScoped<CustomAuthStateProvider>();
-builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IReportService, ReportService>();
 
 builder.Services.AddScoped<IEmployeesService, EmployeesService>();
