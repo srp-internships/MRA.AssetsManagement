6374c88b6409e0ab592d56ae215c79f82884a828
AlisherMavlonov <135858632+AlisherMavlonov@users.noreply.github.com>
2024-04-17 15:53:29 +0500

    89 purchase report (#25)
    * Deleted DocumentsController.cs
    
    * 93 Resolved the conflict
    
    * 89 Added the ability to get a report
    
    * 89 Fixed the warning
    
    * 89 Removed the AssetType field
    
    * Add Report.razor
    
    * Аixed the warning
    
    * Fixed the ReportService.cs
    
    * Remove Fetch
    
    * 89-Fixed date filtering and added an icon
    
    * 89-Fixed the warning
    
    * 95-Clear URL after login and fixed CallBack
    
    * 89-Added date to table
    
    * 89-Added a change to the table
    
    ---------
    
    Co-authored-by: Abbos Sidiqov <abbosidiqov@outlook.com>

---
+++ src/Application/Features/Purchase/Queries/GetPurchasedAssetsQuery.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using MRA.AssetsManagement.Application.Data;
+using MRA.AssetsManagement.Web.Shared.Purchas;
+
+namespace MRA.AssetsManagement.Application.Features.Purchase.Queries;
+
+public class GetPurchasedAssetsQuery(PurchasedAssetsRequest purchasedAssetsRequest) : IRequest<List<GetPurchasedAssets>>
+{
+    public readonly PurchasedAssetsRequest PurchasedAssetsRequest = purchasedAssetsRequest;
+} 
+public class GetPurchasedAssetsQueryHandler(IApplicationDbContext context)
+        : IRequestHandler<GetPurchasedAssetsQuery, List<GetPurchasedAssets>>
+    {
+        public async Task<List<GetPurchasedAssets>> Handle(GetPurchasedAssetsQuery request, CancellationToken cancellationToken)
+        {
+            var documents = await context.Documents.GetAllAsync(x=>
+                x.Date.Date >= request.PurchasedAssetsRequest.FromDate!.Value.Date
+                && x.Date.Date <= request.PurchasedAssetsRequest.ToDate!.Value.Date.AddDays(1),cancellationToken);
+
+            var assetsTypes = await context.AssetTypes.GetAllAsync(cancellationToken);
+            
+            List<GetPurchasedAssets> response = new();
+            
+            foreach (var document in documents)
+            {
+                foreach (var detail in document.Details)
+                {
+                    var asset = new GetPurchasedAssets()
+                    {
+                        AssetName = detail.Asset.Name,
+                        Price = detail.Price,
+                        Quantity = detail.Quantity,
+                        DateTime = document.Date
+                    };
+                    if (request.PurchasedAssetsRequest.AssetTypeId == null)
+                    {
+                        asset.AssetType = assetsTypes.FirstOrDefault(x => x.Id == detail.Asset.AssetTypeId)?.Name;
+                    }
+
+                    if (detail.Asset.AssetTypeId == request.PurchasedAssetsRequest.AssetTypeId)
+                    {
+                        asset.AssetType = assetsTypes.FirstOrDefault(x => x.Id == detail.Asset.AssetTypeId)?.Name;
+                    }
+                    response.Add(asset);
+                }
+            }
+            return response;
+        }
+    }
---
+++ src/Web/Server/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MRA.AssetsManagement.Application.Features.Purchase.Queries;
+using MRA.AssetsManagement.Web.Shared.Purchas;
+
+namespace MRA.AssetsManagement.Web.Server.Controllers;
+
+[Authorize]
+public class ReportController : ApiControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ReportController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<GetPurchasedAssets>>> GetPurchases([FromQuery] PurchasedAssetsRequest purchasedAssetsRequest,CancellationToken cancellationToken)
+    {
+        return await _mediator.Send(new GetPurchasedAssetsQuery(purchasedAssetsRequest),cancellationToken);
+    }
+}
