511ae6a7ac21dc9a14cdc10f48fd12aeb3de350f
Alisher Mavlonov <amavlonoffs@gmail.com>
2024-04-23 15:37:16 +0500

    Fixed errors

---
+++ src/Domain/Entities/AppVersion.cs
@@ -0,0 +1,9 @@
+﻿using MRA.AssetsManagement.Domain.Common;
+
+namespace MRA.AssetsManagement.Domain.Entities;
+
+public class AppVersion : IEntity
+{
+    public string Id { get; set; }
+    public int Version { get; set; }
+}
