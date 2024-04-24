a7b4629561080b7f1268af460cb8fdbeee1dfbd4
Shuhrat Rahmonov <94813339+shuhratrahmonov146@users.noreply.github.com>
2024-04-23 20:37:10 -0700

    #102.Home-Page (#39)
    * #102.Home-Page
    
    * refactor
    
    * fix some code
    
    ---------
    
    Co-authored-by: shuhratrahmonov146@gmail.com <shuhrat@silkroadprofessionals.com>
    Co-authored-by: Abbos Sidiqov <48281953+SidiqovAbbos@users.noreply.github.com>

---
+++ src/Web/Client/Services/HomeService/IHomeService.cs
@@ -0,0 +1,9 @@
+﻿using MRA.AssetsManagement.Web.Shared.AssetSerials;
+
+namespace MRA.AssetsManagement.Web.Client.Services.HomeService
+{
+    public interface IHomeService
+    {
+        Task<IEnumerable<GetAssetTypeSerial>> Get();
+    }
+}
