b9391d30d94b8da84f4232fe9c47c57c3cf0803d
Firuz <firuzisoboev@gmail.com>
2024-05-01 14:18:28 +0500

    deploy-stages (#51)
    * separate client-server
    
    * add CORS
    
    * fix layers deps
    
    * remove trash
    
    * short hostname
    
    ---------
    
    Co-authored-by: Abbos Sidiqov <48281953+SidiqovAbbos@users.noreply.github.com>

--- src/Web/Client/Services/AssetSerials/AssetSerialService.cs
+++ src/Web/Client/Services/AssetSerials/AssetSerialService.cs
@@ -1,50 +1,52 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
 using MRA.AssetsManagement.Web.Shared.Assets;
 using MRA.AssetsManagement.Web.Shared.AssetSerialHistory;
 using MRA.AssetsManagement.Web.Shared.AssetSerials;
 using MRA.AssetsManagement.Web.Shared.Tags;
 using MRA.BlazorComponents.HttpClient.Services;
 using MRA.BlazorComponents.Snackbar.Extensions;
-
 using MudBlazor;
 
 namespace MRA.AssetsManagement.Web.Client.Services.AssetSerials;
 
-public class AssetSerialService(IHttpClientService httpClient, ISnackbar snackbar, IWebAssemblyHostEnvironment environment) : IAssetSerialService
+public class AssetSerialService(IHttpClientService httpClient, ISnackbar snackbar, IConfiguration configuration)
+    : IAssetSerialService
 {
-    private readonly string _baseAddress = environment.BaseAddress;
-    
+    private readonly string _baseAddress = configuration["AssetsManagementApiBaseAddress"]!;
+
     public async Task<IEnumerable<GetAssetSerialHistory>> GetAssetSerialHistories(string serial)
     {
-        var response = await httpClient.GetFromJsonAsync<IEnumerable<GetAssetSerialHistory>>($"{_baseAddress}api/assetserials/histories/{serial}");
+        var response =
+            await httpClient.GetFromJsonAsync<IEnumerable<GetAssetSerialHistory>>(
+                $"{_baseAddress}assetserials/histories/{serial}");
         snackbar.ShowIfError(response, "Occured some errors");
         return response.Result!;
     }
 
     public async Task<GetAssetSerial> GetBySerial(string serial)
     {
-        var response = await httpClient.GetFromJsonAsync<GetAssetSerial>($"{_baseAddress}api/assetserials/{serial}");
+        var response = await httpClient.GetFromJsonAsync<GetAssetSerial>($"{_baseAddress}assetserials/{serial}");
         snackbar.ShowIfError(response, "Occured some errors");
         return response.Result!;
     }
 
     public async Task<List<GetTag>> SetTagsToAssetSerial(SetTagsToAssetSerialsRequest request)
     {
-        var response = await httpClient.PatchAsJsonAsync<List<GetTag>>($"{_baseAddress}api/assetserials/set-tag", request);
+        var response = await httpClient.PatchAsJsonAsync<List<GetTag>>($"{_baseAddress}assetserials/set-tag", request);
         snackbar.ShowIfError(response, "Occured some errors");
         return response.Result!;
     }
 
     public async Task UpdateSerial(UpdateAssetSerialRequest request)
     {
-        var response = await httpClient.PutAsJsonAsync($"{_baseAddress}api/assets", request);
+        var response = await httpClient.PutAsJsonAsync($"{_baseAddress}assets", request);
         snackbar.ShowIfError(response, "Occured some errors");
     }
 
     public async Task<IEnumerable<GetAssetSerial>> GetAssetSerials()
     {
-        var response = await httpClient.GetFromJsonAsync<IEnumerable<GetAssetSerial>>($"{_baseAddress}api/assetserials/serial");
+        var response =
+            await httpClient.GetFromJsonAsync<IEnumerable<GetAssetSerial>>($"{_baseAddress}assetserials/serial");
         snackbar.ShowIfError(response, "Error was occured");
         return response.Result!;
     }
