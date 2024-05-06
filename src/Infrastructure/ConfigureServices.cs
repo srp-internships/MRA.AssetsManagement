4fb196969187e5fd3b201d032e85167651131b1c
firuz2006 <firuzisoboev@gmail.com>
2024-05-06 19:46:50 +0500

    test AllowCredentials

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -17,26 +17,26 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services,
         ConfigurationManager configuration)
-    { 
+    {
         services.AddSingleton<MongoDbMigration>();
         services.AddSingleton<IApplicationDbContext, MongoDbContext>();
         services.AddSingleton<IDataSeeder, MongoDbDataSeeder>();
         services.AddScoped<IEmployeeService, EmployeeService>();
-        
+
         services.AddAuthorization(auth =>
         {
             auth.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
                 .RequireAuthenticatedUser()
                 .Build();
-            
+
             auth.AddPolicy(ApplicationPolicies.Administrator, op => op
                 .RequireRole(ApplicationClaimValues.SuperAdministrator, ApplicationClaimValues.Administrator));
         });
         services.AddScoped<ICurrentUserService, CurrentUserService>();
-        
-        
+
+
         services.AddHttpClient();
-        
+
         services.Configure<MongoDbOption>(configuration.GetSection("MongoDb"));
         var corsAllowedHosts = configuration.GetSection("MraAssetsManagement-CORS").Get<string[]>();
         services.AddCors(options =>
@@ -44,7 +44,7 @@ public static class ConfigureServices
             options.AddPolicy("CORS_POLICY", policyConfig =>
             {
                 policyConfig.WithOrigins(corsAllowedHosts!)
-                    .AllowAnyHeader()
+                    .AllowAnyHeader().AllowCredentials()
                     .AllowAnyMethod();
             });
         });
